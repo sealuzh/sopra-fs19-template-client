f4c6e73c0292c713b13a17d9469d7dbca7192180
dpinezich <david.pinezich@gmail.com>
2019-02-15 08:50:23 +0100

    added prod url for chain testing

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -6,7 +6,7 @@ import { isProduction } from "./isProduction";
  * @returns {string}
  */
 export const getDomain = () => {
-  const prodUrl = ""; // TODO: insert heroku prod url for server (once deployed)
+  const prodUrl = "https://sopra-fs19-admingroup-server.herokuapp.com"; // TODO: insert your groups heroku prod url for server (once deployed)
   const devUrl = "http://localhost:8080";
   if (isProduction()) {
     return prodUrl;
